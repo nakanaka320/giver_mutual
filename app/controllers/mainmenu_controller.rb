e39d108fa4ad9ea3d6b742c3b6fb7b72b04ca56d
nakanaka320 <nakanakamitsuo@icloud.com>
2020-06-07 06:55:25 +0900

    トップメニューなど仮枠完成

---
+++ app/controllers/mainmenu_controller.rb
@@ -0,0 +1,9 @@
+class MainmenuController < ApplicationController
+
+  def index
+  end
+
+  def about
+  end
+
+end
---
+++ test/controllers/mainmenu_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MainmenuControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
