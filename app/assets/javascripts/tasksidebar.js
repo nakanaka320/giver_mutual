4dd68b95350176cc8458edee2b49b63fd3a66b30
nakanaka320 <nakanakamitsuo@icloud.com>
2020-10-16 11:48:41 +0900

    .taskbar_piechart_indexの弟ノードは取得できた、要素も取得できる

--- app/assets/javascripts/tasksidebar.js
+++ app/assets/javascripts/tasksidebar.js
@@ -8,19 +8,40 @@ $(document).on('click', '#open_close', function(){
           if($aside.hasClass('open')){
               $aside.stop(true).animate({right: '0px'}, duration, 'easeOutBack');
               $asidButton.find('img').attr('src', '/assets/btn_close.png');
-// パイチャートの表示
+
+// タスクの種類を定義
+              var Individual = 50;
+              var Work = 20;
+              var Others = 80;
+
+// task-dataを読み取るまでの過程
+              var list = document.querySelectorAll('.taskbar_piechart_index')
+              var opts = list;
+              for (var i = 0, len = opts.length; i < len; i++){
+                var opt = opts.item(i);
+                if (opt.nodeType === 1){
+
+
+                  var test = opt.nextSibling; //optsと一緒
+
+                  console.log(test);
+                  var test2 = test.nextElementSibling;
+                  console.log(test2);
+                }
+              }
+
+// パイチャート表示部分の要素を定義
               var NodeMyChart = document.querySelectorAll(".chart");
               var HashMyChart = Array.from(NodeMyChart);
               var count = 0;
               var IndexChart = "#myChart";
-              var Individual = 56;
-              var Work = 26;
-              var Others = 88;
+
+//チャート作成・表示
               HashMyChart.forEach(function(item,index){
                 // ここにIndexChart+countでdata代入
-              // Individual;
-              // Work;
-              // Others;
+              Individual;
+              Work;
+              Others;
               new Chart(document.querySelector(IndexChart+count), {
                 type: "pie",
                 data: {
@@ -39,7 +60,6 @@ $(document).on('click', '#open_close', function(){
               });
             count++;
             });
-
           }else{
               $aside.stop(true).animate({right: '-1200px'}, duration, 'easeInBack');
               $asidButton.find('img').attr('src', '/assets/btn_open.png');
